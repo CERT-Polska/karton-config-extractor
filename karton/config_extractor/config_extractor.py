0ebeb6d1b79b0dec674bf460a2dd71280ad8e80a
nazywam <michalpr@cert.pl>
2020-12-22 11:25:57 +0100

    Update meta

--- karton/config_extractor/config_extractor.py
+++ karton/config_extractor/config_extractor.py
@@ -8,8 +8,9 @@ import re
 import tempfile
 import zipfile
 
-from karton.core import Karton, Resource, Task, Config
+from karton.core import Config, Karton, Resource, Task
 from malduck.extractor import ExtractManager, ExtractorModules
+
 from .__version__ import __version__
 
 
@@ -17,6 +18,7 @@ class AnalysisExtractManager(ExtractManager):
     """
     Patched version of original ExtractManager, providing current karton interface
     """
+
     def __init__(self, karton: Karton) -> None:
         super(AnalysisExtractManager, self).__init__(karton.modules)
         self.karton = karton
@@ -30,6 +32,7 @@ class ConfigExtractor(Karton):
     """
     Extracts configuration from samples and Drakmon analyses
     """
+
     identity = "karton.config-extractor"
     version = __version__
     persistent = True
@@ -38,34 +41,32 @@ class ConfigExtractor(Karton):
             "type": "sample",
             "stage": "recognized",
             "kind": "runnable",
-            "platform": "win32"
+            "platform": "win32",
         },
         {
             "type": "sample",
             "stage": "recognized",
             "kind": "runnable",
-            "platform": "win64"
+            "platform": "win64",
         },
         {
             "type": "sample",
             "stage": "recognized",
             "kind": "runnable",
-            "platform": "linux"
+            "platform": "linux",
         },
-        {
-            "type": "analysis",
-            "kind": "drakrun-prod"
-        },
-        {
-            "type": "analysis",
-            "kind": "drakrun"
-        }
+        {"type": "analysis", "kind": "drakrun-prod"},
+        {"type": "analysis", "kind": "drakrun"},
     ]
 
     @classmethod
     def args_parser(cls):
         parser = super().args_parser()
-        parser.add_argument("--modules", help="Malduck extractor modules directory", default="extractor/modules")
+        parser.add_argument(
+            "--modules",
+            help="Malduck extractor modules directory",
+            default="extractor/modules",
+        )
         return parser
 
     @classmethod
@@ -90,34 +91,37 @@ class ConfigExtractor(Karton):
         del legacy_config["family"]
 
         # This allows us to spawn karton tasks for special config handling
-        if 'store-in-karton' in legacy_config:
+        if "store-in-karton" in legacy_config:
             self.log.info("Karton tasks found in config, sending")
 
-            for karton_task in legacy_config['store-in-karton']:
-                task_data = karton_task['task']
-                payload_data = karton_task['payload']
-                payload_data['parent'] = parent or sample
+            for karton_task in legacy_config["store-in-karton"]:
+                task_data = karton_task["task"]
+                payload_data = karton_task["payload"]
+                payload_data["parent"] = parent or sample
 
                 task = Task(headers=task_data, payload=payload_data)
                 self.send_task(task)
                 self.log.info("Sending ripped task %s", task.uid)
 
-            del legacy_config['store-in-karton']
+            del legacy_config["store-in-karton"]
 
         if len(legacy_config.items()) == 1:
             self.log.info("Final config is empty, not sending it to the reporter")
             return
 
-        task = Task({
-            "type": "config",
-            "kind": "static",
-            "family": config["family"],
-            "quality": self.current_task.headers.get("quality", "high")
-        }, payload={
-            "config": legacy_config,
-            "sample": sample,
-            "parent": parent or sample
-        })
+        task = Task(
+            {
+                "type": "config",
+                "kind": "static",
+                "family": config["family"],
+                "quality": self.current_task.headers.get("quality", "high"),
+            },
+            payload={
+                "config": legacy_config,
+                "sample": sample,
+                "parent": parent or sample,
+            },
+        )
         self.send_task(task)
 
     # analyze a standard, non-dump sample
@@ -137,23 +141,25 @@ class ConfigExtractor(Karton):
     # analyze a drakrun analysis
     def analyze_drakrun(self, sample, path):
         extractor = create_extractor(self)
-        dumps_path = os.path.join(path, 'dumps')
+        dumps_path = os.path.join(path, "dumps")
         dump_candidates = {}
 
         results = {
-            'analysed': 0,
-            'crashed': 0,
+            "analysed": 0,
+            "crashed": 0,
         }
 
         analysis_dumps = sorted(os.listdir(dumps_path))
         for i, dump in enumerate(analysis_dumps):
             # catch only dumps
-            if re.match(r'^[a-f0-9]{4,16}_[a-f0-9]{16}$', dump):
-                results['analysed'] += 1
-                self.log.debug("Analyzing dump %d/%d %s", i, len(analysis_dumps), str(dump))
+            if re.match(r"^[a-f0-9]{4,16}_[a-f0-9]{16}$", dump):
+                results["analysed"] += 1
+                self.log.debug(
+                    "Analyzing dump %d/%d %s", i, len(analysis_dumps), str(dump)
+                )
                 dump_path = os.path.join(dumps_path, dump)
 
-                with open(dump_path, 'rb') as f:
+                with open(dump_path, "rb") as f:
                     dump_data = f.read()
 
                 if not dump_data:
@@ -169,7 +175,7 @@ class ConfigExtractor(Karton):
                         dump_candidates[family] = (dump, dump_data)
                 except Exception:
                     self.log.exception("Error while extracting from {}".format(dump))
-                    results['crashed'] += 1
+                    results["crashed"] += 1
 
                 self.log.debug("Finished analysing dump no. %d", i)
 
@@ -178,17 +184,20 @@ class ConfigExtractor(Karton):
             dump, dump_data = dump_candidates[family]
             self.log.info("* (%s) %s => %s", family, dump, json.dumps(config))
             parent = Resource(name=dump, content=dump_data)
-            task = Task({
-                "type": "sample",
-                "stage": "analyzed",
-                "kind": "dump",
-                "platform": "win32",
-                "extension": "exe"
-            }, payload={
-                "sample": parent,
-                "parent": sample,
-                "tags": ["dump:win32:exe"]
-            })
+            task = Task(
+                {
+                    "type": "sample",
+                    "stage": "analyzed",
+                    "kind": "dump",
+                    "platform": "win32",
+                    "extension": "exe",
+                },
+                payload={
+                    "sample": parent,
+                    "parent": sample,
+                    "tags": ["dump:win32:exe"],
+                },
+            )
             self.send_task(task)
             self.report_config(config, sample, parent=parent)
 
@@ -204,22 +213,27 @@ class ConfigExtractor(Karton):
         elif headers["type"] == "analysis" and headers["kind"] == "drakrun-prod":
             analysis = task.get_resource("analysis")
             # replace with analysis.get_size(self.minio) once !8 is live
-            if self.minio.stat_object(analysis.bucket, analysis.uid).size > 1024 * 1024 * 128:
+            if (
+                self.minio.stat_object(analysis.bucket, analysis.uid).size
+                > 1024 * 1024 * 128
+            ):
                 self.log.info("Analysis is too large, aborting")
                 return
 
             with analysis.extract_temporary() as fpath:
-                with open(os.path.join(fpath, 'sample.txt'), "r") as f:
+                with open(os.path.join(fpath, "sample.txt"), "r") as f:
                     sample_hash = f.read()
 
-                self.log.info("Processing drakmon analysis, sample: {}"
-                              .format(sample_hash))
+                self.log.info(
+                    "Processing drakmon analysis, sample: {}".format(sample_hash)
+                )
                 self.analyze_drakrun(sample, fpath)
         elif headers["type"] == "analysis" and headers["kind"] == "drakrun":
             # DRAKVUF Sandbox (codename: drakmon OSS)
             sample_hash = hashlib.sha256(sample.content).hexdigest()
-            self.log.info("Processing drakmon OSS analysis, sample: {}"
-                          .format(sample_hash))
+            self.log.info(
+                "Processing drakmon OSS analysis, sample: {}".format(sample_hash)
+            )
             # HACK: We can't use extract_temporary because 'dumps.zip' is pushed as
             # regular resource, not DirectoryResource.
             with tempfile.TemporaryDirectory() as tmpdir:
